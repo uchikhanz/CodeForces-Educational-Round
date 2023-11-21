3783fa0f44f1d613906d3eb9a08f32a87545782f
uchikhanz <86999947+uchikhanz@users.noreply.github.com>
2023-11-21 14:16:24 +0800

    Create A. Tricky Sum.cpp

---
+++ Round 1/A. Tricky Sum.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define forn(i, n) for(int (i) = 0; (i) < (n); (i)++)
+
+int main(){
+	int T;
+  cin >> T;
+	while(T--){
+		long long n;
+		cin >> n;
+		long long ans = (n + 1) * n / 2LL;
+		int pw = 0, res = n;
+		while(res > 0){
+			res /= 2;
+			pw++;
+		}
+		long long current = 1LL;
+		while(pw--) current *= 2LL;
+		ans -= 2 * (current - 1);
+		cout << ans << '\n';
+	}
+}
